0d47eff22c0dcd1172a213a1f6c7357d9983147c
t.nagano.g <t.nagano.g@gmail.com>
2022-06-17 09:03:15 +0900

    日付ピッカー実装

--- lib/view/screens/pages/calender_page.dart
+++ lib/view/screens/pages/calender_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:intl/intl.dart';
 
 class CalenderPage extends StatelessWidget {
   @override
@@ -9,15 +11,40 @@ class CalenderPage extends StatelessWidget {
           title:const Text("カレンダー"),
           centerTitle: true,
           ),
-        body: OutlinedButton(
+        body: Row(
+          children: <Widget> [
+            OutlinedButton(
                 onPressed: () {},
                 style: OutlinedButton.styleFrom(
                   primary: Colors.black,
                 ),
                 child: const Text('今日'),
               ),
+            IconButton(
+              icon: Icon(Icons.calendar_today),
+              onPressed: () async {
+                DatePicker.showDatePicker(
+                  context,
+                  showTitleActions: true,
+                  // onChanged内の処理はDatepickerの選択に応じて毎回呼び出される
+                  onChanged: (date) {
+                      // print('change $date');
+                  }, 
+                  // onConfirm内の処理はDatepickerで選択完了後に呼び出される
+                  onConfirm: (date) {
+            
+                  }, 
+                  // Datepickerのデフォルトで表示する日時
+                  currentTime: DateTime.now(),
+                  locale: LocaleType.jp,
+                  // localによって色々な言語に対応
+                  //  locale: LocaleType.en
+                );
+              },
+            ),
+          ]
         ),
-        
+      ),
     );
   }
 }
